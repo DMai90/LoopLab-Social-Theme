0f689517cd2ead0100f24d4ce778834689162196
David <dmai90@yahoo.com>
2019-02-03 23:33:49 -0800

    Added navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Navbar.css';
+
+const Navbar = props => {
+  return (
+    <nav
+      class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top"
+      id="main-nav"
+    >
+      <div class="container">
+        <a href="index.html" class="navbar-brand">
+          LoopLAB
+        </a>
+        <button
+          class="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#home" class="nav-link">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#explore-head-section" class="nav-link">
+                Explore
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#create-head-section" class="nav-link">
+                Create
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="#share-head-section" class="nav-link">
+                Share
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
